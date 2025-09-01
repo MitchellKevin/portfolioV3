7a5dbd6d6517a2558712dc904e01eb1fea610f6d
mitchellkevin <mitchell.scholte04@gmail.com>
2025-09-01 11:09:48 +0100

    project-structure responveniss added

--- script.js
+++ script.js
@@ -16,46 +16,46 @@ if (featureSection && featureCards.length) {
     featureObserver.observe(featureSection);
 }
 
-const floatImages = [
-    'img/tools/VMB.png',
-    'img/tools/Burp_Suite.png',
-    'img/tools/Sony_alpha.png',
-    'img/tools/VSC.png',
-    'img/tools/nmap.png',
-    'img/tools/Metasploit.png',
-    'img/tools/git.png',
-    'img/tools/DaVinci_Resolve_Studio.png',
-    'img/tools/Lightroom.png',
-    'img/tools/Linux.png',
-    'img/tools/Xcode.png',
-    'img/tools/Android_Studio.png',
-    'img/tools/Photoshop.png',
-    'img/tools/Kali_Linux.png',
-    'img/tools/Figma.png',
-    'img/tools/IntelliJ.png'
-];
+// const floatImages = [
+//     'img/tools/VMB.png',
+//     'img/tools/Burp_Suite.png',
+//     'img/tools/Sony_alpha.png',
+//     'img/tools/VSC.png',
+//     'img/tools/nmap.png',
+//     'img/tools/Metasploit.png',
+//     'img/tools/git.png',
+//     'img/tools/DaVinci_Resolve_Studio.png',
+//     'img/tools/Lightroom.png',
+//     'img/tools/Linux.png',
+//     'img/tools/Xcode.png',
+//     'img/tools/Android_Studio.png',
+//     'img/tools/Photoshop.png',
+//     'img/tools/Kali_Linux.png',
+//     'img/tools/Figma.png',
+//     'img/tools/IntelliJ.png'
+// ];
 
-function showRandomImages() {
-    const container = document.getElementById('project-float-images');
-    container.innerHTML = '';
-    for (let i = 0; i < 5; i++) { // Show 5 images
-        const img = document.createElement('img');
-        img.src = floatImages[Math.floor(Math.random() * floatImages.length)];
-        img.className = 'project-float-img';
-        img.style.top = Math.random() * 80 + 10 + 'vh';
-        img.style.left = Math.random() * 80 + 10 + 'vw';
-        container.appendChild(img);
-    }
-}
+// function showRandomImages() {
+//     const container = document.getElementById('project-float-images');
+//     container.innerHTML = '';
+//     for (let i = 0; i < 5; i++) { // Show 5 images
+//         const img = document.createElement('img');
+//         img.src = floatImages[Math.floor(Math.random() * floatImages.length)];
+//         img.className = 'project-float-img';
+//         img.style.top = Math.random() * 80 + 10 + 'vh';
+//         img.style.left = Math.random() * 80 + 10 + 'vw';
+//         container.appendChild(img);
+//     }
+// }
 
-function hideRandomImages() {
-    document.getElementById('project-float-images').innerHTML = '';
-}
+// function hideRandomImages() {
+//     document.getElementById('project-float-images').innerHTML = '';
+// }
 
-document.querySelectorAll('.project_item a').forEach(link => {
-    link.addEventListener('mouseenter', showRandomImages);
-    link.addEventListener('mouseleave', hideRandomImages);
-});
+// document.querySelectorAll('.project_item a').forEach(link => {
+//     link.addEventListener('mouseenter', showRandomImages);
+//     link.addEventListener('mouseleave', hideRandomImages);
+// });
 
 document.addEventListener('DOMContentLoaded', function() {
   const slides = document.querySelectorAll('.cert-slide');
